28c53aca6b7a044dcbfea7618ef922326e4603b2
hiroki-kt <hiroki.katayama@dtosh.com>
2023-11-05 10:33:19 +0900

    change allow host settings

--- back/django/app/settings.py
+++ back/django/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9jv34%^q4a))x@2nk==v5mm&smhizstoeth$ocu4_hip$gol#9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
