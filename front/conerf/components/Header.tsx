d13e313c910573881539d166a456d80bcd87d7b9
Hiroki-kt <hiroki.katayama@dtosh.com>
2023-11-10 02:07:33 +0900

    add home ui

--- front/conerf/components/Header.tsx
+++ front/conerf/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className='h-36 fixed w-full '>
       <div className='h-full w-4/5 m-auto flex items-center'>
-        <div className='text-4xl'>Job List</div>
+        <div className='text-4xl'>CoNeRF Job List</div>
       </div>
     </header>
   )
--- front/conerf/pages/App.tsx
+++ front/conerf/pages/App.tsx
@@ -22,11 +22,19 @@ const App = () => {
 
   return (
     <main className='w-full h-full'>
-      <div className='m-auto pt-14 w-4/5'>
-        <div className='text-xl mt-5'>ジョブ一覧</div>
-        <div className='mt-5'>
+      <div className='m-auto pt-36 w-4/5'>
+        <div className='w-full h-10 rounded-full border border-gray-700 flex items-center px-8 text-gray-700'>
+          Sample....
+        </div>
+        <div className='text-3xl mt-5'>Your Jobs</div>
+        <div className='mt-5 flex flex-wrap justify-between'>
           {jobs.map((job) => {
-            return <div>{job.title}</div>
+            return (
+              <div className='w-1/4 '>
+                <a href='/job' className='block w-full rounded-3xl pt-[100%] bg-gray-700'></a>
+                <div className='mt-3'>{job.title}</div>
+              </div>
+            )
           })}
         </div>
       </div>
