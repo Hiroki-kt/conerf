1751ae11aae928907c86484fb48643e62cc6e431
hiroki-kt <hiroki.katayama@dtosh.com>
2023-11-10 00:40:00 +0900

    add api

--- front/conerf/components/Header.tsx
+++ front/conerf/components/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
-    <header>
-      <h1>Conerf</h1>
+    <header className='h-36 fixed w-full '>
+      <div className='h-full w-4/5 m-auto flex items-center'>
+        <div className='text-4xl'>Job List</div>
+      </div>
     </header>
   )
 }
--- front/conerf/pages/App.tsx
+++ front/conerf/pages/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [jobs, setJobs] = useState([])
+
+  const getJobs = async () => {
+    const res = await fetch('/api/getjobs', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await res.json()
+    console.log(data)
+    setJobs(data)
+  }
+
+  useEffect(() => {
+    getJobs()
+  }, [])
+
   return (
-    <main>
-      <div> サンプル</div>
+    <main className='w-full h-full'>
+      <div className='m-auto pt-14 w-4/5'>
+        <div className='text-xl mt-5'>ジョブ一覧</div>
+        <div className='mt-5'>
+          {jobs.map((job) => {
+            return <div>{job.title}</div>
+          })}
+        </div>
+      </div>
     </main>
   )
 }
